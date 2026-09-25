b0b5e581631d9af1fbff9d38a9244d2976d4ad41
T-Sabrina <sabrinatorres263@gmail.com>
2026-09-25 15:59:53 -0300

    Creacion de funciones de calculo de porcentaje normal, calculo2, calculo3, y monto_fijo

--- core.py
+++ core.py
@@ -19,23 +19,72 @@ class Tratamiento:
         r += f"|Id algoritmo:{self.idalgoritmo:<4}"
         return r
 
-def calculo1(tratamientos):
-    n = len(tratamientos)
+def porcentaje_normal(Tratamiento):
+
+    punto = Tratamiento.icd10.find(".")
+    porcentaje_extra = int(Tratamiento.icd10[punto + 1:]) / 100
+
+    return porcentaje_extra
+
+#Falta terminar
+def monto_fijo(Tratamiento):
+    n = len(Tratamiento)
+
+    for i in range(n):
+        bloque = Tratamiento.icd10[i].find()
+        if "A" <= Tratamiento.icd10[i] <= "L":
+            monto_fijo = 20000
+
+        elif "M" <= Tratamiento.icd10[i] <= "P":
+            monto_fijo = 15000 + 5000
+
+def calculo1(Tratamiento):
+    n = len(Tratamiento)
     suma_fija = 0
     porcentaje_extra = 0
 
     for i in range(n):
-        letra = tratamientos.icd10[i][0]
-        punto = tratamientos.icd10[i].find(".")
+        letra = Tratamiento.icd10[i][0]
 
-        if tratamientos.montobase[i] > 60000:
-            porcentaje_extra = int(tratamientos.icd10[punto + 1:]) / 100
-            if tratamientos.complejidad[i] == "A" and letra != "U":
-                suma_fija = tratamientos.montobase[i] / 2
+        if Tratamiento.montobase[i] > 60000:
+            porcentaje_extra = porcentaje_normal(Tratamiento)
 
-        monto_final = tratamientos.montobase[i] + porcentaje_extra + suma_fija
+            if Tratamiento.complejidad[i] == "A" and letra != "U":
+                suma_fija = Tratamiento.montobase[i] / 2
+
+        monto_final = Tratamiento.montobase[i] + porcentaje_extra + suma_fija
 
         return monto_final
+
+def calculo2(Tratamiento):
+    n = len(Tratamiento)
+
+    for i in range(n):
+        letra = Tratamiento.icd10[i][0]
+        punto = Tratamiento.icd10[i].find(".")
+
+        if "A" <= letra <= "P":
+            porcentaje_extra = porcentaje_normal(Tratamiento)
+
+        elif "Q" <= letra <= "Z":
+            if Tratamiento.complejidad[i] == "A":
+                porcentaje_extra = (int(Tratamiento.icd10[punto + 1:]) * 2) / 100
+
+            elif Tratamiento.complejidad[i] == "R":
+                porcentaje_extra = Tratamiento.montobase[i] * 0.15
+
+        monto_final = Tratamiento.montobase[i] + porcentaje_extra
+
+        return monto_final
+
+def calculo3(Tratamiento):
+    n = len(Tratamiento)
+    monto_extra = 0
+
+    for i in range(n):
+        if Tratamiento.complejidad[i] == "A":
+            monto_extra = Tratamiento.montobase[i] * 0.30
+
 
 def proces_linea(linea: str):
     data = []
